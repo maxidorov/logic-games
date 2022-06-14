b088b311003dc0f55dd3a678d97e952cb3912090
Maxim Sidorov <s_maxim_v@bk.ru>
2022-06-14 17:26:01 +0300

    init xcode proj

---
+++ LogicGames/LogicGamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  LogicGamesApp.swift
+//  LogicGames
+//
+//  Created by MSP on 14.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct LogicGamesApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
